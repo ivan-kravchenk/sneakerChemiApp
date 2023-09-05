e95e6f58a03b829b56bf8e6e70254045f8fb21da
ivan <ivan.ivab@icloud.com>
2023-09-05 13:07:15 +0300

    Initial Commit

---
+++ SneackerChemi/SneackerChemiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneackerChemiApp.swift
+//  SneackerChemi
+//
+//  Created by Tribuna_159 on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SneackerChemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
